42698712e89912840ebe06235b91a77edafd0672
99Rafick <rafraf9727@gmail.com>
2024-08-18 23:00:28 +0100

    set project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,vue}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        primary: '#9C7962'
+      },
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1100px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
