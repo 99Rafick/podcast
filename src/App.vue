bcf4b59e5230a6ef6bf1cc173d33daa276564d06
99Rafick <rafraf9727@gmail.com>
2024-08-18 23:01:29 +0100

    create podcast card component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 <template>
   <main class="w-screen relative h-screen overflow-hidden">
       <div class="w-full h-full flex items-center justify-center bg-slate-900">
-          
+          <PodcastCard></PodcastCard>
       </div>
   </main>
 </template>
 
 <script setup>
+import PodcastCard from '@/components/PodcastCard.vue'
 </script>
---
+++ src/components/PodcastCard.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="h-96 w-56 bg-white">
+
+    </div>
+</template>
+
+<script>
+    
+</script>
