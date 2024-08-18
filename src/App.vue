d0f097d4055b804c54a349dceee92e34e2cd0394
99Rafick <rafraf9727@gmail.com>
2024-08-19 00:53:50 +0100

    horizontal scroll & set active in middle

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,64 @@
 
 <template>
   <main class="w-screen relative object-cover h-screen">
-      <div id="content" class="w-[7000px] relative h-full bg-slate-900">
-          <!-- <PodcastCard></PodcastCard>
-          <PodcastCard></PodcastCard>
-          <PodcastCard></PodcastCard>
-          <PodcastCard></PodcastCard>
-          <PodcastCard id="box1" :active="true"></PodcastCard>
-          <PodcastCard></PodcastCard>
-          <PodcastCard></PodcastCard>
-          <PodcastCard></PodcastCard>
-          <PodcastCard></PodcastCard> -->
-
-          <div  class="box1 w-56 z-[3] absolute top-1/2 left-[1200px] -translate-y-1/2 h-56 bg-white"></div>
+      <div id="content" class="w-max pl-[calc(50%-10rem)] pr-[calc(50%-8rem)] flex items-center justify-center relative h-full bg-slate-900">
+          <PodcastCard v-for="(card, index) in cards" 
+                    :key="index" 
+                     ref="podcastCards" 
+                    :active="card.active">
+          </PodcastCard>
       </div>
   </main>
 </template>
 
 <script setup>
-// import PodcastCard from '@/components/PodcastCard.vue'
+import { ref, reactive, onMounted } from 'vue';
+import PodcastCard from '@/components/PodcastCard.vue';
 import gsap from 'gsap';
-import ScrollTrigger from "gsap/ScrollTrigger";
-import { onMounted } from 'vue';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger)
+const cards = reactive([
+  { active: false },
+  { active: false },
+  { active: false },
+  { active: false },
+  { active: false },
+  { active: false },
+  { active: false },
+  { active: false },
+  { active: false },
+]);
 
 onMounted(() => {
-  const content = document.querySelector('#content')
+  const content = document.querySelector('#content');
+  const podcastCards = ref([]);
 
+  podcastCards.value = document.querySelectorAll('#content .card');
 
-  let scrollTween = gsap.to(content, {
+  gsap.to(content, {
     xPercent: -100,
     x: () => window.innerWidth,
-    ease: "none", // <-- IMPORTANT!
+    ease: 'none',
     scrollTrigger: {
-      pin: "main",
-      trigger: "main",
-      start: "left left",
-      end: () => `+=${content.offsetWidth} bottom`,
+      pin: 'main',
+      trigger: 'main',
+      start: 'left left',
+      end: () => `+=${content.offsetWidth} top`,
       scrub: 1,
       markers: true,
-    }
-  });
+      onUpdate: () => {
+        const midScreenX = window.innerWidth / 2;
 
-  const tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".box1",
-        start: "left 70%",
-        end: "left 20%",
-        scrub: 1,
-        containerAnimation: scrollTween,
-        markers: true
+        podcastCards.value.forEach((card, index) => {
+          const cardRect = card.getBoundingClientRect();
+          const cardCenterX = cardRect.left + (cardRect.width / 2);
+
+          cards[index].active = Math.abs(midScreenX - cardCenterX) < cardRect.width / 2;
+        });
+      },
     },
+  });
 });
-
-tl.to(".box1", {
-    rotation: 360,
-    duration: 1
-});
- 
-
-
-})
 </script>
--- src/components/PodcastCard.vue
+++ src/components/PodcastCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="['h-96 w-72 flex-none cursor-pointer rounded-3xl overflow-hidden relative', 
+    <div :class="['h-96 w-72 transition duration-300 ease-out card z-10 flex-none cursor-pointer rounded-3xl overflow-hidden relative', 
         {'bg-white scale-[0.85]': !active, 'bg-purple-600': active}
     ]">
         <div class="h-16 w-full translate-y-full  transition duration-150 ease-in-out bg-white absolute bottom-0">
