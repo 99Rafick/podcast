6cab8250ad93f8492cae3d5723c2274c2aee8b14
99Rafick <rafraf9727@gmail.com>
2024-08-19 03:09:30 +0100

    ending

--- src/App.vue
+++ src/App.vue
@@ -6,14 +6,16 @@
              background-position: 50%;
       ;">
       <div class="w-full z-0 h-full absolute bg-black/60 backdrop-blur-2xl"></div>
-      <div id="content" class="w-max z-10 pl-[calc(50%-10rem)] gap-6 pr-[calc(50%-8rem)] flex items-center justify-center relative h-full">
-          <PodcastCard v-for="(card, index) in cards" 
+      <div id="content" class="w-max z-10 pl-[15%] pr-[90%] bg-  sm:pl-[30%] sm:pr-[40%]  md:pl-[calc(50%-10rem)] gap-5 2xl:gap-6 md:pr-[calc(50%-8rem)] flex items-center justify-center relative h-full">
+          <Card v-for="(card, index) in cards" 
                     :key="index"
                     ref="podcastCards" 
                     :name="card.name"
                     :image="card.image"
+                    :color="card.color"
+                    :number="index+1"
                     :active="card.active">
-          </PodcastCard>
+          </Card>
 
           
       </div>
@@ -22,22 +24,22 @@
 
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
-import PodcastCard from '@/components/PodcastCard.vue';
+import Card from '@/components/Card.vue';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const cards = reactive([
-  {name: 'Elea MILLER', active: false, image: "https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-  {name: 'James Cornet', active: false, image: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-  {name: 'Elea Miller', active: false, image: "https://images.pexels.com/photos/3187171/pexels-photo-3187171.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
-  {name: 'Elea Miller', active: false, image: "https://images.pexels.com/photos/2179241/pexels-photo-2179241.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
-  {name: 'Elea Miller', active: false, image: "https://images.pexels.com/photos/2180525/pexels-photo-2180525.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
-  {name: 'Elea Miller', active: false, image: "https://images.pexels.com/photos/2006726/pexels-photo-2006726.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
-  {name: 'Elea Miller', active: false, image: "https://images.pexels.com/photos/2301285/pexels-photo-2301285.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
-  {name: 'Elea Miller', active: false, image: "https://images.pexels.com/photos/2973552/pexels-photo-2973552.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
-  {name: 'Elea Miller', active: false, image: "https://images.pexels.com/photos/1125328/pexels-photo-1125328.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+  {color: 'bg-blue-900', name: 'Elea MILLER', active: false, image: "https://images.pexels.com/photos/1081685/pexels-photo-1081685.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+  {color: 'bg-white', name: 'James CORNET', active: false, image: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+  {color: 'bg-yellow-500', name: 'Amina GRAYCE', active: false, image: "https://images.pexels.com/photos/3187171/pexels-photo-3187171.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
+  {color: 'bg-stone-800', name: 'Gary BRAWN', active: false, image: "https://images.pexels.com/photos/2179241/pexels-photo-2179241.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
+  {color: 'bg-amber-900', name: "Madeline N'DOE", active: false, image: "https://images.pexels.com/photos/2180525/pexels-photo-2180525.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
+  {color: 'bg-yellow-600', name: 'Jocelyn MED', active: false, image: "https://images.pexels.com/photos/2006726/pexels-photo-2006726.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
+  {color: 'bg-white', name: 'Aya KANTE', active: false, image: "https://images.pexels.com/photos/2301285/pexels-photo-2301285.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
+  {color: 'bg-black', name: 'John DOE', active: false, image: "https://images.pexels.com/photos/2973552/pexels-photo-2973552.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"},
+  {color: 'bg-teal-500', name: 'Maya JONES', active: false, image: "https://images.pexels.com/photos/1125328/pexels-photo-1125328.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"},
 ]);
 
 onMounted(() => {
---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="relative h-max">
+        <div :class="['h-96 w-80 2xl:h-[480px] 2xl:w-[390px] transition duration-500 ease-out card z-1000 flex-none cursor-pointer rounded-3xl overflow-hidden relative', 
+            {'scale-[0.8] 2xl:scale-[0.70]': !active, 'bg-purple-600': active}
+        ]">
+
+            <img :class="['w-full h-full object-cover', {'grayscale': !active}]" :src="image" alt="">
+
+        
+            <div :class="[color, 'h-16 2xl:h-20 w-full  transition justify-center flex items-center px-3 duration-300 ease-in-out absolute bottom-0', 
+                {'translate-y-[150%]': !active}
+            ]"> 
+                    <span :class="['flex items-center font-black text-xl lg:text-3xl', 
+                        {'text-white': color !== 'bg-white'}]">
+                        Number {{ number}}
+                    </span>
+                
+            </div>
+        </div>
+
+        <div :class="['absolute w-full text-center left-1/2 mt-14 2xl:mt-16 -translate-x-1/2 transition-all duration-400 ease-in-out', {'translate-y-16 opacity-0': !active, 'opacity-100': active}]">
+            <span :class="[color, 'block w-max mx-auto mb-2 text-sm px-4 py-1 rounded-xl opacity-80', {'text-white': color !== 'bg-white'}]">Lorem ipsum dolor sit.</span>
+            <span class="text-3xl sm:text-4xl 2xl:text-5xl top-0 font-semibold text-white/90">{{ name }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        active: {
+            type: Boolean,
+            value: () => false,
+        },
+        image: String,
+        name: String,
+        color: String,
+        number: Number
+    })
+</script>
